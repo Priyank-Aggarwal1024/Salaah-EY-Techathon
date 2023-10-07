14dcf7a714c57c7ab5fd0c40317c98bb5217a526
Priyank Aggarwal <priyankaggarwal6395@gmail.com>
2023-10-07 23:11:23 +0530

    final commit

---
+++ eyTechathonData.js
@@ -0,0 +1,207 @@
+const obj =[{
+    id: 6,
+    company: "EY TECHATHON",
+    cover: cover,
+    about: (
+      <div>
+        <b> EY Techathon 4.0 | Re-imagining Possibilities with GenAI</b>
+        <br />
+        <br />
+        EY Techathon 4.0 is open to undergraduate and postgraduate students (part-time or full-time) from any discipline in any college based out of India. Participants can register individually or in teams with at most 5 members. Cross-specialization and cross-college teams are allowed.
+      </div>
+    ),
+    // problem: (
+    //   <div>
+    //     <li>
+    //     What's in it for students?
+    //     </li>
+    //     <li>→ Global ranking to top coders</li>
+    //     <li>→ Top 3 coders to win total a prize money of USD 20,000/-</li>
+    //     <li>→ A chance to explore exciting careers* with one of the world's most powerful brand</li>
+    //     <li>→ A chance to compete with some of the best coders in the world</li>
+    //     <li>→ A platform to showcase your programming skills</li>
+    //     <li>→ The finalists stand a chance to travel** to India for the live grand finale experience.</li>
+    //   </div>
+    // ),
+    eligibility : (
+        <div>
+            <h2>Eligibility Criteria</h2>
+            <p>
+                Eligibility for Participants: The following eligibility criteria shall be applicable for participation
+                in the Competition:
+                <br />
+                <br />
+            </p>
+            <li>
+                • The Participant(s) should be an Indian citizen, residing in India only and above 18 years of age on
+                the date of the participation
+            </li>
+            <li>
+                • The Participant(s) should be students currently enrolled in a degree/diploma course (part-time or
+                    full-time) from any discipline in any college based out of India
+            </li>
+            <li>
+                • The Participant(s) can participate either in their individual capacity or as a team with team size not
+                exceeding 4 members at a time
+            </li>
+            <li>
+                • The Participant(s) can have maximum of two non-students in their team provided that the students
+                form the majority of the team composition
+            </li>
+            <li>
+                • The Participant should not have a criminal conviction or an arrangement or a contract that prevents
+                the Participant from participating in the Competition;
+            </li>
+            <li>
+                • The Participant should have proof of age and address proof when requested by EY and must be
+                mandatorily submitted by the Participant to EY, as may be required by EY to confirm the eligibility
+                of the Participant;
+            </li>
+            <li>
+                • The Participant should abide by all the applicable laws at all times, including complying with the
+                lockdown order and other orders, instructions and guidelines issued by any government authority,
+                judicial or quasi-judicial authority;
+            </li>
+            <li>
+                • Employees, directors, partners of EY and its affiliates are not eligible to participate in this
+                Competition;
+            </li>
+            <li>
+                • The Participant(s) should be an Indian citizen, residing in India only and above 18 years of age on
+                the date of the participation
+            </li>
+            <li>
+                • In the event the Participant fulfils all the criteria above, then he/she/they may submit the Entries in
+                accordance with these Terms and Conditions. EY reserves the right but not the obligation to verify
+                if an individual meets all the criteria. EY reserves the sole right to disqualify and reject the Entries
+                and registration of Participant(s) at any point of time in the event the above criteria or any part
+                thereof is not met with or any discrepancy is found to exist in the information furnished/provided
+                by the Participant at any point of time
+            </li>
+        </div>
+    ),
+    process: (
+      <div>
+        <h2> How do I register and login for the contest?</h2>
+        <br />
+        <br />
+        <li>
+          • The Participant(s) who are desirous of entering the Competition are required to register at
+            www.ey.com/en_in/techathon-3 (“Microsite”) indicating the personal details of the individual or
+            ofeach team member.
+        </li>
+        <li>
+          • Once the Participant(s) accesses the Microsite, the Participant(s) shall be required to share the
+            Personal Information by duly filling the personal details including but not limited to full name, age,
+            city, college id, email ID, mobile phone number(s) and/or any other information/documentation as
+            sought by EY, at its discretion (“Personal Information”) in a manner and form, as requested and
+            /or prescribed on the Microsite
+        </li>
+        <li>• For avoidance of doubt, for the purposes of participating in the Competition set out hereinabove,
+            registering for the Competition through a valid mobile phone number and email id via the medium
+            of the Microsite is mandatory for all interested Participants. EY is not responsible to validate
+            correctness of the mobile number and email id provided by the Participant(s)</li>
+        <li>• Then choose India or Rest of the World, as applicable</li>
+        <li>
+          • If at any point of time EY finds that the mobile phone number, email id or any other information
+            provided by the Participant(s) including on the Microsite, is false and/or unauthenticated, then EY
+            shall have the right to forthwith disqualify such Participant from further participation and his/her
+            Project shall immediately be considered to be null and void
+        </li>
+        <li>
+          • The interested Participant(s) shall carefully read the Terms and Conditions, complete filling details
+            of the Personal Information and click the checkbox 'I Accept Terms & Conditions' in order to
+            accept the Terms and Conditions and submission of the Personal Information.
+        </li>
+        <li>
+          • The Participant(s) agrees and undertakes that all Personal Information, provided by them at the
+            time of registration is true, correct, and accurate.
+        </li>
+        <br />
+        <br />
+        <b> Round 1(Executive Summary Submission):</b>
+        <br />
+        <br />
+        <ul>
+          <li>• Only registered students will be able to access the problem statements from the round active.</li>
+          <li>
+            • Teams will be required to submit a 1-3 slider PPT (.pdf format) of their proposed solution in the prescribed format.
+          </li>
+          <li>
+            • The submission templates can be accessed by the registered students/teams from the round titled "Round 1: Executive Summary Submission" on this page.
+          </li>
+          <li>
+            • All the participants are requested to submit their solutions well before the deadline to avoid any last-minute hassle.
+          </li>
+          <li>
+            • Based on the submissions received, the top teams will be shortlisted and taken to the next round.
+          </li>
+        </ul>
+        <br />
+        <br />
+        <ul>
+          <li>
+            <b>Round 2 (Semi-Final: Detailed Presentation Submission):</b>
+            <br />• The shortlisted teams from Round 1 will upload their detailed presentation on the chosen problem statement.
+            <br />• Teams will be required to submit their detailed presentation in a deck with less than or equal to 10 slides.
+            <br />• The presentation should be structured and based on the presentation duration i.e. 10 minutes.
+
+          </li>
+          <br />
+          <li>
+            <b>Semi Finale Presentations:</b>
+            <br />
+            • Shortlisted participants will present their solution models in this round to a jury panel comprising EY leaders. Based on this round, finalists will be announced.
+          </li>
+          <br />
+          <li>
+            <b>Mentorship:</b>
+            <br />
+            • The shortlisted teams will work with their respective mentors to prepare their innovative submissions for the Grand Final.
+            <br />• This is a great opportunity for teams to get some expert guidance on any roadblocks they may be facing or simply to just gain some confidence by pitching EY leaders.
+          </li>
+          <br />
+          <li>
+            <b>Round 3 ( Grand Finale):</b>
+            <br />
+            • The Gen AI use cases will be presented by the finalists to a jury comprising industry leaders in the grand finale and the winners will be announced.
+          </li>
+          <br />
+          <br />
+        </ul>
+      </div>
+    ),
+    judgingCriteria: (
+      <div>
+        <h3>The Projects shall be evaluated and shortlisted by a Screening Jury based on the following criteria:</h3>
+        <ul>
+          <li>→ Uniqueness of the idea</li>
+          <li>→ Design of the solution</li>
+          <li>→ Solution design framework and use of technology</li>
+          <li>→ End-user benefit and social impact</li>
+          <li>→ Based on the scores received from the Screening Jury, some Projects shall be shortlisted for
+                evaluation by the Semi-Finals and Grand Finale Jury</li>
+          <li>→ The semi-finalists and finalists will then be required to make a virtual presentation to the SemiFinals and Grand Finale 
+                Jury post which the winners shall be decided based on the scores provided
+                by the Final Jury ('Winner')</li>
+          <li>→ The Jury shall have the sole authority to judge and evaluate the Projects and their decision shall be
+                final and binding on all Participants</li>
+        </ul>
+      </div>
+    ),
+    reward: (
+      <div>
+        <li>→ Winner - Prizes worth INR 3,00,000</li>
+        <li>→ Runner-Up - Prizes worth INR 2,00,000</li>
+        <li>
+          → Exciting cash prizes up for grabs.
+        </li>
+        <li>→ Internship opportunities with EY.</li>
+        <li>→ Mentoring with EY and industry stalwarts.</li>
+      </div>
+    ),
+  },
+  {
+
+  }
+]
